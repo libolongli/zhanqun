95635dba074943dae6aefe3a2f76754ba4376dc9
nomius <libolongli@gmail.com>
2018-05-04 17:46:04 +0800

    初始化站群代码
    初始化站群代码

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+return array (
+  'haha.com' => 
+  array (
+    'table_prefix' => 'hahacom_',
+    'title' => 'haha',
+    'domain' => 'www.haha.com',
+  ),
+  'fifalive614.com' => 
+  array (
+    'table_prefix' => 'fifalive614com_',
+    'title' => 'fifalive614',
+    'domain' => 'www.fifalive614.com',
+  ),
+  'gfifa2018.com' => 
+  array (
+    'table_prefix' => 'gfifa2018com_',
+    'title' => '国际足球',
+    'domain' => 'www.gfifa2018.com',
+  ),
+  'soccerm2018.com' => 
+  array (
+    'table_prefix' => 'soccerm2018com_',
+    'title' => '足球大师',
+    'domain' => 'soccerm2018.com',
+  ),
+  'grey.com' => 
+  array (
+    'table_prefix' => 'greycom_',
+    'title' => 'grey是运维',
+    'domain' => 'www.grey.com',
+  ),
+);
+?>
---
+++ cron_zhandian.sh
@@ -0,0 +1,3 @@
+sh /www/zhanqun/zhandian.sh 
+echo ''>/www/zhanqun/zhandian.sh 
+chmod 777 -R /www/zhanqun
